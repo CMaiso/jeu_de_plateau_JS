3dd0e35b3dfcf427898a06d14503bc3783fbdc43
Camille Maisonobe <48532925+CMaiso@users.noreply.github.com>
2019-07-09 15:50:50 +0200

    Delete html.js

--- Projet 6 - Jeu de Plateau/js/html.js
+++
@@ -1,9 +0,0 @@
-// //Elements dynamiques de la page HTML
-// import Personnage from './personnages.js';
-// import Plateau from './plateau.js';
-//
-// let J1 = this.listeJoueurs[0];
-// let J2 = this.listeJoueurs[1];
-//
-//     $('#statsJ1').append(`<li>${J1.vie}</li>`).append(`<li>${J1.arme.skin}</li>`).append(`<li>${J1.arme.degats}</li>`);
-//
